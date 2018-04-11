c4efc28e6f869a816f8f6a45ac67f6440c515d88
janus_wel <janus.wel.3@gmail.com>
2018-04-11 23:37:45 +0900

    :wrench::heavy_plus_sign: add react-native-dotenv

--- dotenv/App.js
+++ dotenv/App.js
@@ -1,37 +1,22 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * Sample React Native App to show configurations in bundled file
  */
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { API_TOKEN } from 'react-native-dotenv';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text style={styles.text}>
+          {API_TOKEN}
         </Text>
       </View>
     );
@@ -45,12 +30,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
+  text: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
